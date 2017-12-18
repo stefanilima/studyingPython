0d100285e45ccb591b0b211f6676541b77a6adff
Stefani Lima <stefani.lima@kinghost.com.br>
2017-12-18 17:56:08 -0200

    Exericios 1, 2 e 3 do python Brasil

---
+++ exerciciosPythonBrasil/1_EstruturaSequencial/exercicio3.py
@@ -0,0 +1,4 @@
+# 3 - Faça um Programa que peça dois números e imprima a soma.
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+print('A soma dos dois números é: ', num1 + num2)
